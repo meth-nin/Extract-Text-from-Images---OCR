d2a96d2c5548825f5c28e11d003b5c4985925907
meth-nin <meth2468nin@gmail.com>
2026-01-06 14:15:49 +0530

    Preprocess function

--- ocr.py
+++ ocr.py
@@ -1,13 +1,13 @@
-from PIL import Image
 import pytesseract
+from preprocess import preprocess_image
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Users\Methupa-Thyaga\AppData\Local\Programs\Tesseract-OCR\tesseract.exe"
 
-image = Image.open("23a93669523149a78a1d20153fc74610.jpg")
+image_path = "23a93669523149a78a1d20153fc74610.jpg"
 
-gray_image = image.convert("L")
+processed_image = preprocess_image(image_path)
 
-text = pytesseract.image_to_string(gray_image)
+text = pytesseract.image_to_string(processed_image)
 
 clean_text = text.replace("\x0c", "").strip()
 print(clean_text)
--- preprocess.py
+++ preprocess.py
@@ -1,27 +1,19 @@
 from PIL import Image, ImageEnhance
-import pytesseract
-import cv2
-import numpy as np
 
-pytesseract.pytesseract.tesseract_cmd = r"C:\Users\Methupa-Thyaga\AppData\Local\Programs\Tesseract-OCR\tesseract.exe"
+def preprocess_image(image_path):
+    img = Image.open(image_path)
 
-img = Image.open("23a93669523149a78a1d20153fc74610.jpg")
+    # grayscale
+    gray_img = img.convert("L")
 
-#gray scale
-gray_img = img.convert("L")
+    # resize
+    width, height = gray_img.size
+    gray_img = gray_img.resize((width * 2, height * 2), Image.LANCZOS)
 
-#resize
-width, height = gray_img.size
-gray_img_re = gray_img.resize((width*2, height*2), Image.LANCZOS)
+    # increase contrast
+    gray_img = ImageEnhance.Contrast(gray_img).enhance(2.0)
 
-#contrast
-gray_img_re = ImageEnhance.Contrast(gray_img_re).enhance(2.0)
+    # binarization
+    bw = gray_img.point(lambda x: 0 if x < 140 else 255, '1')
 
-#binarization
-bw = gray_img_re.point(lambda x: 0 if x < 140 else 255, '1')
-
-#ocr
-text = pytesseract.image_to_string(bw)
-
-clean_text = text.replace("\x0c", "").strip()
-print(clean_text)
+    return bw
